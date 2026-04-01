a64dbbee54c0b1d9c51da550d5d01ff362854c99
R-Venkat-Kalyan <2100030959cseh@gmail.com>
2026-04-01 22:47:57 +0530

    resident ui pages done, minimal changes pending

--- src/main/java/com/hms/meenakshi/controller/ResidentController.java
+++ src/main/java/com/hms/meenakshi/controller/ResidentController.java
@@ -16,6 +16,30 @@ public class ResidentController {
         return "resident-pages/layout";
     }
 
+    @GetMapping("/accommodation")
+    public String accommodation(Model model){
+        model.addAttribute("mainContent", "resident-pages/accommodation");
+        return "resident-pages/layout";
+    }
+
+    @GetMapping("/my-payments")
+    public String myPayments(Model model){
+        model.addAttribute("mainContent", "resident-pages/my-payments");
+        return "resident-pages/layout";
+    }
+
+    @GetMapping("/add-fee-receipt")
+    public String addFeeReceipt(Model model){
+        model.addAttribute("mainContent", "resident-pages/add-receipts");
+        return "resident-pages/layout";
+    }
+
+    @GetMapping("/fee-receipts")
+    public String myFeeReceipts(Model model){
+        model.addAttribute("mainContent", "resident-pages/view-receipts");
+        return "resident-pages/layout";
+    }
+
     @GetMapping("/add-laundry")
     public String addLaundry(Model model){
         model.addAttribute("fullName", "Kalyan");
